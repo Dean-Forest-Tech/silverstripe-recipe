de696c8091a99a607dc58c8940104736caadf42d
Makreig <mark@i-lateral.com>
2018-06-20 15:22:20 +0100

    added image deferring

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,2 +1,8 @@
 <?php
  
+
+use SilverStripe\View\Parsers\ShortcodeParser;
+use ilateral\SilverStripe\DeferedImages\DeferedImageShortcodeProvider;
+
+ShortcodeParser::get('default')
+    ->register('image', [DeferedImageShortcodeProvider::class, 'handle_shortcode']);
---
+++ themes/custom/templates/DBFile_image.ss
@@ -0,0 +1,1 @@
+<img class="image-fluid w-100" src="$MicroImage.URL.ATT" alt="$Title.ATT" data-src="$URL.ATT" />
