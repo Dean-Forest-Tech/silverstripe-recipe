4540273391aa45445518d3d1599274563ebb602e
Morven Lewis-Everley <morven@i-lateral.com>
2022-07-19 22:53:59 +0100

    Setup webpack and linting requirements

---
+++ themes/custom/webpack.prod.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = merge(
+  common,
+  {
+    mode: 'production',
+
+    plugins: [
+      new CleanWebpackPlugin({
+          cleanOnceBeforeBuildPatterns: [Path.join(__dirname, "dist/**/*")],
+      })
+    ]
+  }
+);
