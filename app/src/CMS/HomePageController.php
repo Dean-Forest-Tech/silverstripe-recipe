10a03c95cc8c3bb1e82747966abf1c8c75e3a632
Morven Lewis-Everley <morven@i-lateral.com>
2022-08-17 14:18:56 +0100

    Add more template options for homepage sections

--- app/src/CMS/HomePageController.php
+++ app/src/CMS/HomePageController.php
@@ -3,6 +3,7 @@
 namespace App\CMS;
 
 use PageController;
+use ReflectionClass;
 use SilverStripe\ORM\ArrayList;
 
 class HomePageController extends PageController
@@ -15,12 +16,22 @@ class HomePageController extends PageController
             ->sort('HomeSort','ASC');
 
         foreach ($pages as $page) {
-            $page->Layout = $page->renderWith([
+            $templates = $page->getViewerTemplates();
+            $base_class = (new ReflectionClass($page))->getShortName();
+
+            $templates = [
+                [
+                    'type' => 'Includes',
+                    $base_class . '_homepage',
+                    'Page_homepage',
+                    'HomePage_section'
+                ],
                 $page->ClassName.'_homepage',
                 'Layout/Page_homepage',
                 'HomePage_section'
-            ]);
-            
+            ];
+
+            $page->Layout = $page->renderWith($templates);
             $sections->push($page);
         }
 
